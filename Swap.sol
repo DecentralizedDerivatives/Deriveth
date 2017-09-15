41ad390b25ecfd9ff571f33573ff23b50ce0084b
themandalore <fett.nicholas@yahoo.com>
2017-09-15 17:16:37 -0400

    Update Swap.sol

--- Swap.sol
+++ Swap.sol
@@ -2,19 +2,20 @@ pragma solidity ^0.4.13;
 
 import "../contracts/Oracle.sol";
 
+//This is the swap contract itself
 contract Swap {
   enum SwapState {created,open,started,ready,ended}
-  SwapState public currentState;
-  address public long_party;
-  address public short_party;
-  uint public notional;
-  uint public lmargin;
-  uint public smargin;
-  address public oracleID;
-  bytes32 public startDate;
-  bytes32 public endDate;
-  address public creator;
-  uint public cancel;
+  SwapState public currentState; //state of the swap, open can be entered by opposing party
+  address public long_party;//Party going long the rate
+  address public short_party;//Party short the rate
+  uint public notional;//This is the amount that the change will be calculated on.  10% change in rate on 100 Ether notional is a 10 Ether change
+  uint public lmargin;//The amount the long party puts as collateral (max he can lose, most short party can gain)
+  uint public smargin;//The amount the short party puts as collateral (max he can lose, most long party can gain)
+  address public oracleID;//The Oracle address (check for list at www.github.com/DecentralizedDerivatives/Oracles)
+  bytes32 public startDate;//Start Date of Swap - is the hex representation of the date variable in YYYYMMDD
+  bytes32 public endDate;//End Date of Swap - is the hex representation of the date variable in YYYYMMDD
+  address public creator;//The Factory it was created from
+  uint public cancel;//The cancel variable.  If 0, no parties want to cancel.   Once Swap is started, both parties can cancel.  If cancel =1, other party is trying to cancel
   address party;
   bool long;
 
@@ -24,6 +25,7 @@ modifier onlyState(SwapState expectedState) {require(expectedState == currentSta
 
 Oracle d;
 
+  //this base function is run by the Factory
   function Swap(address OAddress, address _cpty1, address _creator){
       d = Oracle(OAddress);
       oracleID = OAddress;
@@ -32,7 +34,10 @@ Oracle d;
       currentState = SwapState.created;
     
   }
-  
+
+  //this function is where you enter the details of your swap.  
+  //The Eligble Contract Participant variable (ECP) verifies that the party self identifies as eligible to enter into a swap based upon their jurisdiction
+  //Be sure to send your collateral (margin) while entering the details.
   function CreateSwap(bool ECP, uint _margin, uint _margin2, uint _notional, bool _long, bytes32 _startDate, bytes32 _endDate) onlyState(SwapState.created) payable {
       require(ECP);
       require (msg.sender == party);
@@ -52,6 +57,9 @@ Oracle d;
       startDate = _startDate;
   }
   mapping(address => bool) paid;
+  //This function is for those entering the swap.  
+  //Needing to enter the details of the swap a second time ensures that your counterparty cannot modify the terms right before you enter the swap. 
+  //Note you do not need to enter your collateral as a variable, however it must be submitted with the contract
   function EnterSwap(bool ECP, uint _margin, uint _notional, bool _long, bytes32 _startDate, bytes32 _endDate ) onlyState(SwapState.open) payable returns (bool) {
       require(ECP);
       require(_long == long && notional == _notional && _startDate == startDate && _endDate == endDate);
@@ -71,7 +79,7 @@ Oracle d;
   
 
   mapping(uint => uint) shares;
-
+//This function calculates the payout of the swap.  Note that the value of the underlying cannot reach zero, but get within .001 * the precision of the Oracle
     function Caculate() onlyState(SwapState.started) returns (bool){
     uint p1=div(mul(1000,RetrieveData(endDate)),RetrieveData(startDate));
         if (p1 == 1000){
@@ -96,7 +104,7 @@ Oracle d;
   }
   
 
-
+//Once calcualted, this function allows each party to withdraw their share of the collateral.
   function PaySwap() onlyState(SwapState.ready) returns (bool){
   if (msg.sender == long_party && paid[long_party] == false){
         paid[long_party] = true;long_party.send(shares[1]);
@@ -107,6 +115,9 @@ Oracle d;
     if (paid[long_party] && paid[short_party]){currentState = SwapState.ended;}
     return true;
   }
+
+//This function allows both parties to exit.  If only the creator has entered the swap, then the swap can be cancelled and the details modified
+//Once two parties enter the swap, the contract is null after cancelled
 
   function Exit(){
     require(currentState != SwapState.ended);
@@ -144,6 +155,7 @@ Oracle d;
 
   struct DocumentStruct{bytes32 name; uint value;}
 
+//If you want to check if the Calculate function can be run, enter the hex of the date in the RetrieveData field and see if it returns a non-zero value
   function RetrieveData(bytes32 key) public constant returns(uint) {
     DocumentStruct memory doc;
     (doc.name,doc.value) = d.documentStructs(key);
@@ -167,3 +179,13 @@ Oracle d;
   }
 
   function sub(uint256 a, uint256 b) internal returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
