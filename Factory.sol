d8e2027dc8adff6e9f3daaacd4bdf541661ec36f
themandalore <fett.nicholas@yahoo.com>
2017-09-15 17:17:08 -0400

    Update Factory.sol

--- Factory.sol
+++ Factory.sol
@@ -2,29 +2,32 @@ pragma solidity ^0.4.13;
 
 import "../contracts/Swap.sol";
 
-
+//The Factory contract creates the individual swap contracts
 contract Factory {
     address[] public newContracts;
     address public creator;
-    address public oracleID;
-    uint public fee;
+    address public oracleID; //address of Oracle
+    uint public fee; //Cost of contract in Wei
+
     modifier onlyOwner{require(msg.sender == creator); _;}
     event Print(address _name, address _value);
 
     function Factory (address _oracleID){
         creator = msg.sender;  
         oracleID = _oracleID;
+        fee = 10000000000000000;
     }
     /*ie .01 ether = 1000 */
     function setFee(uint _fee) onlyOwner{
       fee = _fee *10000000000000;
     }
 
+    //This is the function participants will call.  Pay the fee, get returned your new swap address
     function createContract () payable returns (address){
         require(msg.value >= fee);
         address newContract = new Swap(oracleID,msg.sender,creator);
         newContracts.push(newContract);
-        Print(msg.sender,newContract);
+        Print(msg.sender,newContract); //This is an event and allows DDA/ participants to see when new contracts are pushed.
         return newContract;
     } 
     function withdrawFee() onlyOwner {
